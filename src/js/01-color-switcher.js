422fff008569831c6d98f22c8c6e278adf650168
OnishYurii <onishchenkoyurii@gmail.com>
2023-10-24 17:40:53 +0300

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId;
 
+startBtn.addEventListener('click', renderBackground);
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});
+
+function renderBackground() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
